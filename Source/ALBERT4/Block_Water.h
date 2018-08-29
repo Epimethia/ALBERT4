66aae6d979295bb3bf1b68362f1cb827e95081ea
MDS\Jasper.Lyo7552 <jjlyonsschool@gmail.com>
2018-08-29 12:46:40 +1200

    Created bunch of box classes

---
+++ Source/ALBERT4/Block_Water.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Block_NotWalkable.h"
+#include "Block_Water.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALBERT4_API ABlock_Water : public ABlock_NotWalkable
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets default values for this actor's properties
+	ABlock_Water();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	TArray<AActor*> OverlappingActors;
+	
+	
+};
