7bdd977f52e12d5e6747bbab7d9cab758fb72886
James Devenport <jaden.links@yahoo.com>
2018-09-05 01:14:22 +1200

    Improved stevens movement a little bit to take into accoutn unwalkable terrain

--- Source/ALBERT4/Block_Crumble.cpp
+++ Source/ALBERT4/Block_Crumble.cpp
@@ -3,10 +3,25 @@
 #include "Block_Crumble.h"
 
 
-
-
 ABlock_Crumble::ABlock_Crumble() {
 	BlockMesh->SetStaticMesh(ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Assets/Blocks/Crumble_Block/Crumble_Block_Mesh.Crumble_Block_Mesh'")).Object);
+	
+	Trigger = CreateDefaultSubobject<UBoxComponent>(TEXT("BlockTrigger"));
+	Trigger->SetBoxExtent(FVector(50.0f, 50.0f, 50.0f), true);
+	Trigger->SetRelativeLocation(FVector(0.0f, 0.0f, 150.0f));
+	Trigger->SetVisibility(true);
+	Trigger->SetupAttachment(RootComponent);
+	Trigger->bDynamicObstacle = true;
+	Trigger->bGenerateOverlapEvents = true;
+
+	FScriptDelegate DelegateBegin;
+	DelegateBegin.BindUFunction(this, "OnTestOverlapBegin");
+	Trigger->OnComponentBeginOverlap.Add(DelegateBegin);
+
+	FScriptDelegate DelegateEnd;
+	DelegateEnd.BindUFunction(this, "OnTestOverlapEnd");
+	Trigger->OnComponentEndOverlap.Add(DelegateEnd);
+
 	SetActorLocation(FVector(0.0f, 0.0f, 0.0f));
 	SetActorTickEnabled(true);
 }
@@ -20,6 +35,14 @@ void ABlock_Crumble::Fall() {
 	BlockMesh->SetRelativeScale3D(FVector(0.995f, 0.995f, 0.995f));
 	BlockMesh->SetCollisionProfileName(FName("OverlapAll"));
 	BlockMesh->SetSimulatePhysics(true);
+}
+
+void ABlock_Crumble::OnOverlapBegin(AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult){
+	UE_LOG(LogTemp, Warning, TEXT("Colliding"));
+}
+
+void ABlock_Crumble::OnOverlapEnd(AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex){
+	UE_LOG(LogTemp, Warning, TEXT("No Longer Colliding"));
 }
 
 
--- Source/ALBERT4/Block_Crumble.h
+++ Source/ALBERT4/Block_Crumble.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Block_Walkable.h"
 #include "TimerManager.h"
+#include "Components/BoxComponent.h"
 #include "Block_Crumble.generated.h"
 
 /**
@@ -22,6 +23,13 @@ public:
 	void Crumble();
 	void Fall();
 
+	UFUNCTION()
+	void OnOverlapBegin(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 private:
 	FTimerHandle FallTimer;
+	UBoxComponent* Trigger;
 };
