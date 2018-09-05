4257b429503cafbe7ed140cd8fedf0e8559efae1
PlagueDoctorJasperLyons <jjlyonsschool@gmail.com>
2018-09-05 22:54:17 +1200

    oops

--- Source/ALBERT4/Block_Start.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Block_Start.h"
-
-ABlock_Start::ABlock_Start() {
-	BlockMesh->SetStaticMesh(ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Assets/Blocks/Start_Block/Start_Block_Mesh.Start_Block_Mesh'")).Object);
-}
-
