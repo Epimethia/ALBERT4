f0ac2513fa959a48c8ed6e88797e287db6ba0ae6
James Devenport <jaden.links@yahoo.com>
2018-09-05 14:34:22 +1200

    Added a number of meshes to the project

--- Source/ALBERT4/Block_Environment.cpp
+++ Source/ALBERT4/Block_Environment.cpp
@@ -3,11 +3,11 @@
 #include "Block_Environment.h"
 
 
-ABlock_Environment::ABlock_Environment() {
+ABlock_Environment_0::ABlock_Environment_0() {
 	//This block is not walkable
 	Walkable = false;
 
 	//Setting the block mesh
-	BlockMesh->SetStaticMesh(ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Assets/Blocks/Environment_Block/Environment_Block_Mesh.Environment_Block_Mesh'")).Object);
+	BlockMesh->SetStaticMesh(ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Assets/Blocks/Environment_Block/Environment_Block_0_Mesh.Environment_Block_0_Mesh'")).Object);
 	BlockMesh->SetCanEverAffectNavigation(false);
 }
