349fbfa9e256575cb0b2fb265bfc0b2f6e29ad70
PlagueDoctorJasperLyons <jjlyonsschool@gmail.com>
2018-10-15 14:19:55 +1300

    Implemented log box collider

--- Source/ALBERT4/Block_Log.cpp
+++ Source/ALBERT4/Block_Log.cpp
@@ -7,8 +7,18 @@ ABlock_Log::ABlock_Log()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	FVector scalingVector = FVector(0.75f, 0.75f, 0.80f); // Vector used to calculate the correct scaling for the box collider and mesh
+
+	// Setup box collider
+	BoxCollider = CreateDefaultSubobject<UBoxComponent>(FName("BoxCollider"));	
+	BoxCollider->AttachToComponent(BlockMesh, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+	BoxCollider->SetBoxExtent(FVector(-50.0f, -50.0f, 50.0f) * (FVector(2.0f, 2.0f, 2.0f) - scalingVector));		
+	BoxCollider->SetCollisionProfileName(FName("BlockAll"));	
+
+	// Setup collision
 	BlockMesh->SetCollisionProfileName(FName("BlockAll"));
-	BlockMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel1, ECollisionResponse::ECR_Ignore);
+	BlockMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel1, ECollisionResponse::ECR_Ignore);	
+	BlockMesh->SetRelativeScale3D(scalingVector);
 
 	//Setting the block mesh
 	BlockMesh->SetStaticMesh(ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Game/Assets/Blocks/Log_Block/Pushable_log.Pushable_log'")).Object);
